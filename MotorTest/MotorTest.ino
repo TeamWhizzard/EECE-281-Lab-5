8f9d56f69316f2bc36aa2d0837c0d73fba8133ce
John Deppe <johndeppe@gmail.com>
2015-02-11 20:33:11 -0800

    Stepper Motor
    Stepper motor works with Pololu/Allegro A4988 breakout board.
    
    Bracket mount for ultrasound next?

---
+++ MotorTest/MotorTest.ino
@@ -0,0 +1,27 @@
+const int directionPin = 2;
+const int stepPin = 3;
+const int sleepPin = 4;
+const int resetPin = 5;
+const int enablePin = 9;
+
+void setup() {
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(sleepPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  
+  digitalWrite(directionPin, LOW);
+  digitalWrite(stepPin, LOW);
+  digitalWrite(sleepPin, HIGH);
+  digitalWrite(resetPin, HIGH);
+  digitalWrite(enablePin, LOW);
+  delay(100);
+}
+
+void loop() {
+  digitalWrite(stepPin, HIGH);
+  delay(10);
+  digitalWrite(stepPin, LOW);
+  delay(100);
+}
